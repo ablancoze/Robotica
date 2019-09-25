b724abaee60fe20b557def34d0fd847ac16e2398
Robotica <unex.es>
2019-09-25 17:06:37 +0200

    asdasd

---
+++ joystickComp/src/CMakeFiles/joystick.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "joystick_autogen"
+  "CMakeFiles/joystick_autogen.dir/AutogenOldSettings.cmake"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "JoyStick.cpp"
+  "JoyStick.h"
+  "CMakeFiles/joystick.dir/specificworker.cpp.o"
+  "CMakeFiles/joystick.dir/specificmonitor.cpp.o"
+  "CMakeFiles/joystick.dir/home/salabeta/robocomp/classes/qjoystick/qjoystick.cpp.o"
+  "CMakeFiles/joystick.dir/home/salabeta/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/joystick.dir/home/salabeta/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/joystick.dir/home/salabeta/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/joystick.dir/main.cpp.o"
+  "CMakeFiles/joystick.dir/genericmonitor.cpp.o"
+  "CMakeFiles/joystick.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/joystick.dir/genericworker.cpp.o"
+  "CMakeFiles/joystick.dir/joystickI.cpp.o"
+  "CMakeFiles/joystick.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/joystick.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/joystick.dir/GenericBase.cpp.o"
+  "CMakeFiles/joystick.dir/JoyStick.cpp.o"
+  "CMakeFiles/joystick.dir/joystick_autogen/mocs_compilation.cpp.o"
+  "../bin/joystick.pdb"
+  "../bin/joystick"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/joystick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
