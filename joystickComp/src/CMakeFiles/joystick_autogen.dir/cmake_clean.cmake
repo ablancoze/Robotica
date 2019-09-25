4b6e3e86d0c8c89f24ddaf013c73b79786cab41c
Robotica <unex.es>
2019-09-25 16:51:47 +0200

    componente joistic

---
+++ joystickComp/src/CMakeFiles/joystick_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "joystick_autogen"
+  "CMakeFiles/joystick_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/joystick_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/joystick_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
